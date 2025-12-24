e6cfed98d39cf7bbf0d2d12b3bc3bd1345964c6b
Wilma Darc Alves de Farias <wilma.darc@gmail.com>
2025-12-23 22:10:54 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -54,8 +54,6 @@ def main():
 
     # Título principal da aplicação
     st.write('# Análise dos dados de Telemarketing')
-    st.write('# Análise dos dados de Telemarketing')
-    st.write('# Análise dos dados de Telemarketing')
     st.markdown("---")
     
     # Apresenta a imagem na barra lateral da aplicação
